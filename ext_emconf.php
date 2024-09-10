d4fae9b7aba3520b7745afe370f35181bf8b8021
NITSAN <sanjay@nitsan.in>
2024-09-10 20:16:44 +0530

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $EM_CONF['ns_google_sitekit'] = [
-    'title' => 'Official Site Kit by Google',
+    'title' => 'Site Kit by Google for TYPO3',
     'description' => "Know what's going on with your TYPO3 site? How users visit and interact with your pages? Get Every Insights with single click button of Popular Google Tools including Google Analytics, Search Console, Google Ads & AdSense, and Tag Manager to your TYPO3 site without coding. Improve your website's performance and SEO.",
     'category' => 'be',
     'author' => 'T3: Navdeepsinh Jethwa T3: Vishal Solanki',
@@ -10,7 +10,7 @@ $EM_CONF['ns_google_sitekit'] = [
     'state' => 'stable',
     'uploadfolder' => 1,
     'createDirs' => '',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '10.0.0-12.5.99'
